11b89c5256212d360b46bb7bf8f59d183e57d873
= <neymegen50@gmail.com>
2022-09-27 23:14:05 +0300

    init files

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './Calculator.jsx.jsx';
+
+
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Calculator />, rootElement);
